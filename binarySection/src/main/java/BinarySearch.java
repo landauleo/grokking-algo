da8dc4284c37fb3c58d447e7720e094d461f9206
ruanaaf <bambie7@mail.ru>
2019-06-30 23:32:39 +0300

    added BinarySearch class

---
+++ binarySection/src/main/java/BinarySearch.java
@@ -0,0 +1,31 @@
+/**
+ * User: Anna Nosova
+ * Time: 2019-06-30 23:10
+ */
+public class BinarySearch {
+
+    private static int startBinarySearch(int[] sortedArray, int desired) {
+        int low = 0;
+        int high = sortedArray.length - 1;
+        int index = Integer.MAX_VALUE;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (sortedArray[mid] < desired) {
+                low = mid + 1;
+            } else if (sortedArray[mid] > desired) {
+                high = mid - 1;
+            } else if (sortedArray[mid] == desired) {
+                index = mid;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 7, 9, 14};
+
+        System.out.println(startBinarySearch(arr, 9));
+    }
+}
